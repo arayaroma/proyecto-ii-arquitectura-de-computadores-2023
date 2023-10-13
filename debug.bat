6080b88f5a6c2abbfb3071e7ddc5d45dde4b5d47
Daniel Araya <darayaroma@gmail.com>
2023-10-13 17:23:18 -0600

    [ADD] Debug script

---
+++ debug.bat
@@ -0,0 +1,20 @@
+:: remove all files from target
+del target\*.* /Q
+
+:: assemble the files
+bin\tasm /z src\main.asm
+bin\tasm /z src\graphics.asm
+bin\tasm /z src\menu.asm
+bin\tasm /z src\mouse.asm
+
+:: copy bin files into target
+copy *.obj target
+del *.obj
+
+:: link the files
+bin\tlink target\main.obj target\graphics.obj target\menu.obj target\mouse.obj
+
+pause
+
+:: execute it
+bin\td target\main.exe
