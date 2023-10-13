59445b6837e87d414a0189daedec0cf67b7c7fb2
Daniel Araya <darayaroma@gmail.com>
2023-10-13 17:23:49 -0600

    [RENAMED] script.bat -> execute.bat

--- execute.bat
+++ execute.bat
@@ -5,15 +5,16 @@ del target\*.* /Q
 bin\tasm /z src\main.asm
 bin\tasm /z src\graphics.asm
 bin\tasm /z src\menu.asm
+bin\tasm /z src\mouse.asm
 
 :: copy bin files into target
 copy *.obj target
 del *.obj
 
 :: link the files
-bin\tlink target\main.obj target\graphics.obj target\menu.obj
+bin\tlink target\main.obj target\graphics.obj target\menu.obj target\mouse.obj
 
-:: pause
+pause
 
 :: execute it
 target\main.exe
