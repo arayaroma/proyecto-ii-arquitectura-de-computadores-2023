1450d19635dac502b08530818ce25e9a1aaabdb3
chizk0 <126604899+chizk0@users.noreply.github.com>
2023-10-14 01:04:09 -0600

    [FIX] printPoint

--- src/util.asm
+++ src/util.asm
@@ -1,4 +1,16 @@
+
+
 waitEvent macro
-    mov ah, 00H
-    int 16H
+    mov ah, 00H ; wait for keypress
+    int 16H 
+endm
+
+;==============================================================================
+printPoint macro x, y , color
+    mov ah, 0ch
+    mov al,color                   ; color TODO: PASS COLOR BY PARAMETER
+    mov bh,0					; pagina 
+    mov cx, x
+    mov dx, y             
+    int 10h
 endm
