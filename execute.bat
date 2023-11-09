ad088e2ce9a871846cc6fb8c05a4dacb14c87b1f
Daniel Araya <darayaroma@gmail.com>
2023-11-09 12:34:15 -0600

    [Fix] bat file

--- execute.bat
+++ execute.bat
@@ -4,16 +4,17 @@ del target\*.* /Q
 :: assemble the files
 bin\tasm /zi src\main.asm
 bin\tasm /zi src\ascii.asm
-bin\tasm /zi src\graphics.asm
+bin\tasm /zi src\graphic.asm
 bin\tasm /zi src\menu.asm
 bin\tasm /zi src\mouse.asm
 bin\tasm /zi src\board.asm
 bin\tasm /zi src\move.asm
-bin\tasm /zi src\readFile.asm
+bin\tasm /zi src\file.asm
+pause
 bin\tasm /zi src\score.asm
 bin\tasm /zi src\about.asm
 bin\tasm /zi src\option.asm
-bin\tlink /v main.obj ascii.obj graphics.obj menu.obj mouse.obj board.obj move.obj readFile.obj score.obj option.obj about.obj
+bin\tlink /v main.obj ascii.obj graphic.obj menu.obj mouse.obj board.obj move.obj file.obj score.obj option.obj about.obj
 
 :: copy the files
 copy *.obj target
